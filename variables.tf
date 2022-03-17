dd0ae64fcab1d7d0b07958de2f89aaf628717f0e
yyc266 <yanyongchao001@chinasoftinc.com>
2022-03-17 18:08:14 +0800

    2

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "region" {
+  description = "region"
+  default     = "cn-north-4"
+  type        = string
+}
+
+variable "access_key" {
+  description = "access_key"
+  default     = "ZEWNM8J84K3TYGTT7NR1"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "secret_key"
+  default     = "ekFDpoYz8GUb6dbh4v21kqcNGdI43DBWWjf2Gqt5"
+  type        = string
+}
+
 variable "vpc_name" {
   default = "vpc-basic"
 }
